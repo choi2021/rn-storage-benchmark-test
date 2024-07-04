56b2bf767c268df746c6c43e0a01ed3c8545b6b0
Youngjun Choi <juni2004c@gmail.com>
2024-07-04 15:57:03 +0900

    비교 자료 올리기

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,5 @@
-import {Button, SafeAreaView,StyleSheet} from "react-native";
-import React, {useCallback} from "react";
+import {Button, SafeAreaView, StyleSheet, View,Text} from "react-native";
+import React, {useCallback, useState} from "react";
 import {getFromMMKV} from "@/storage/MMKV";
 import {getFromAsyncStorage} from "@/storage/AsyncStorage";
 import {getFromSQLite} from "@/storage/OpSQLite";
@@ -25,39 +25,71 @@ async function benchmark(
         }
         const end = performance.now();
         const diff = end - start;
-        console.log(`Finished Benchmark "${label}"! Took ${diff.toFixed(4)}ms!`);
-        return diff;
+        const result =+diff.toFixed(4)
+        console.log(`Finished Benchmark "${label}"! Took ${result}ms!`);
+        return result;
     } catch (e) {
         console.error(`Failed Benchmark "${label}"!`, e);
         return 0;
     }
 }
 
-async function waitForGC(): Promise<void> {
-    // Wait for Garbage Collection to run. We give a 500ms delay.
-    return new Promise(r => setTimeout(r, 500));
+const getFromStorage=(type:StorageType)=>{
+    switch (type) {
+        case 'op-sqlite':
+            return getFromSQLite;
+        case 'mmkv':
+            return getFromMMKV;
+        case 'async-storage':
+            return getFromAsyncStorage;
+        case 'watermelonDB':
+            return getFromWatermelonDB;
+    }
 }
 
-export default function Index() {
-    const runBenchmarks = useCallback(async () => {
-        console.log('Running Benchmark in 3... 2... 1...');
-        await benchmark('OP-SQLITE         ', getFromSQLite);
-        await waitForGC();
-        await benchmark('MMKV                 ', getFromMMKV);
-        await waitForGC();
-        await benchmark('AsyncStorage         ', getFromAsyncStorage);
-        await waitForGC();
-        await benchmark('WaterMelonDB         ', getFromWatermelonDB);
-    }, []);
+type StorageType = 'op-sqlite' | 'mmkv' | 'async-storage' | 'watermelonDB';
 
+const storageTypes: StorageType[] = ['op-sqlite', 'mmkv', 'async-storage', 'watermelonDB'];
+
+
+
+export default function Index() {
+    const [result,setResult] = useState<Record<StorageType, number>>(
+        storageTypes.reduce((acc, type) => ({...acc, [type]: 0}), {} as Record<StorageType, number>)
+    );
+
+
+
+    const runBenchmarks = useCallback(async () => {
+
+        const values = await Promise.all(storageTypes.map(async (type) => {
+            return benchmark(`${type}       `, getFromStorage(type));
+        }))
+
+        const newResult = storageTypes.reduce((acc, type, index) => ({...acc, [type]: values[index]}), {} as Record<StorageType, number>);
+        setResult(newResult);
+    }, []);
 
     return (
         <SafeAreaView style={styles.container}>
-            <Button title="Run Benchmarks" onPress={runBenchmarks} />
+            <View style={styles.resultContainer}>
+                {Object.keys(result).map((key) => (
+                    <Text key={key} style={styles.resultText}>{key}: {result[key as StorageType]}ms</Text>
+                ))}
+            </View>
+
+            <View>
+                <Button title="Run Benchmarks" onPress={runBenchmarks} />
+            </View>
         </SafeAreaView>
     );
 };
 
 const styles = StyleSheet.create({
-    container: {flex: 1, justifyContent: 'center', alignItems: 'center'},
+    container: {flex: 1, justifyContent: 'space-between', alignItems: 'center'},
+    resultContainer:{
+        justifyContent:'center',
+        flex:1
+    },
+    resultText:{fontSize:20}
 });
